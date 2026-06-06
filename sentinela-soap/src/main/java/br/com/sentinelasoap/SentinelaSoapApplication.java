ed667400c26aff864fd4057904c02d8260bfd6eb
Giulia Barbizan <giuliabarbizan@gmail.com>
2026-06-06 01:27:33 -0300

    feat(soap): setup spring boot application and database config

---
+++ sentinela-soap/src/main/java/br/com/sentinelasoap/SentinelaSoapApplication.java
@@ -0,0 +1,11 @@
+package br.com.sentinelasoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelaSoapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelaSoapApplication.class, args);
+    }
+}
